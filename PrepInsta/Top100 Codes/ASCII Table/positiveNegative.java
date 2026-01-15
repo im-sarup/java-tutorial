a464bea0d2f4c02feb864892bf02e8906601c9b9
Fajle Sarup Hossain <fajlesaruphossain1999@gmail.com>
2026-01-15 21:39:55 +0530

    Commit the OlderFileName to newFileName

---
+++ PrepInsta/Top100 Codes/ASCII Table/positiveNegative.java
@@ -0,0 +1,34 @@
+// Method 1: Brute Force Method
+// public class positiveNegative {
+//     // main class
+
+//     public static void main(String[] args) {
+//         int num = 15;
+
+//         // Condition to check if the number is positive or negative
+//         if (num > 0)
+//             System.out.println("The number is Positive");
+//         else if (num < 0)
+//             System.out.println("The number is Nositive");
+//         else
+//             System.out.println("The number is Zero");
+//     }
+// }
+
+// Method 2: Nested if-else
+public class positiveNegative {
+
+    // main class
+    public static void main(String[] args) {
+    int num = 10;
+    
+    // Condition to check if number is positive or negative
+    if (num >= 0){
+        if (num == 0)
+            System.out.println{"The number is positive"}
+        if(num > 0)
+            System.out.println("The number is Positive");
+    }
+    else
+        System.out.println("The number is Nositive");
+}
